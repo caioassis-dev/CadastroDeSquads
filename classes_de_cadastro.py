d66aa5984acd019cee9521a554e5f24d42723119
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2023-01-11 23:34:32 -0300

    feat(classes_de_cadastro):Criacao de classes para cadastro de pessoas.

---
+++ classes_de_cadastro.py
@@ -0,0 +1,32 @@
+class Pessoa:
+  def __init__(self, nome, fone):
+    self.nome = nome
+    self.fone = fone
+    
+class Squad:
+  def __init__(self,nome, techlead = None, devs = None):
+    self.nome = nome
+    self. devs = []
+    self.techlead = techlead
+    
+  def incluir_techlead(self, techlead):
+    self.techlead = techlead
+    
+class Colaborador (Pessoa):
+  def __init__(self,nome, fone, squad = None):
+    super().__init__(nome, fone)
+    self.squad = squad
+  def incluir_squad(self,squad):
+    self.squad = squad
+    
+class Dev(Colaborador):
+  def __init__(self,nome, fone, cargo, squad = None):
+    super().__init__(nome, fone, squad)
+    self.cargo = cargo
+    
+  def incluir_dev(self, dev):
+    self.devs.append(dev)
+    
+    
+    
+    
