7accb3f1af8e16784c4bd42216bb060ef14e40a0
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2023-01-22 18:49:17 -0300

    refact(classes_de_cadastro):Adicionando funcoes finais de cada classe

--- classes_de_cadastro.py
+++ classes_de_cadastro.py
@@ -2,6 +2,8 @@ class Pessoa:
   def __init__(self, nome, fone):
     self.nome = nome
     self.fone = fone
+  def exibir(self):
+    print(f'-> {self.nome} - {self.fone}')
     
 class Squad:
   def __init__(self,nome, techlead = None, devs = None):
@@ -11,6 +13,9 @@ class Squad:
     
   def incluir_techlead(self, techlead):
     self.techlead = techlead
+    
+  def incluir_dev(self, dev):
+    self.devs.append(dev)
     
 class Colaborador (Pessoa):
   def __init__(self,nome, fone, squad = None):
@@ -23,6 +28,10 @@ class Dev(Colaborador):
   def __init__(self,nome, fone, cargo, squad = None):
     super().__init__(nome, fone, squad)
     self.cargo = cargo
+    
+  def exibir(self):
+    super().exibir()
+    print(f'  Cargo de {self.cargo} na squad {self.squad.nome}\n')
     
   def incluir_dev(self, dev):
     self.devs.append(dev)
